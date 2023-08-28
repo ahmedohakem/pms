b762af07ea3ee87fa3e69e71511f6cb8b3ec033b
Ahmed Hakem <34775534+doingdev@users.noreply.github.com>
2023-08-28 12:55:57 +0200

    Update conn_db.php

--- includefiles/conn_db.php
+++ includefiles/conn_db.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect ("217.199.187.72", "cl53-pms", "BstcBstc@70") or die ('000');
-mysqli_select_db($conn, "cl53-pms") or die ('0000');
+$conn = mysqli_connect ("DB_SERVER_HOST", "DB_SERVER_USERNAME", "DB_SERVER_PASSWORD") or die ('000');
+mysqli_select_db($conn, "DB_NAME") or die ('0000');
 mysqli_query($conn, "SET NAMES utf8;");
 mysqli_query($conn, "SET CHARACTER_SET utf8;");
 ?>
